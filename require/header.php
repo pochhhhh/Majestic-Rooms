617a69bc0000e7154c928dfb644fdc4202ffb590
Marlon <marlonpempengco@gmail.com>
2018-03-13 21:51:41 +0000

    set up a dedciated require folder for the header.php and footer.php template files

---
+++ require/header.php
@@ -0,0 +1,51 @@
+<header>
+
+<section id="navbar">
+
+	<!-- Insert Bootstrap navbar here -->
+
+
+
+	<nav class="navbar navbar-fixed-top navbar-inverse">
+
+	<div class="container-fluid">
+
+	<div class="navbar-header">
+
+
+
+	<a href="/Majestic-Rooms" class="navbar-left"><img src="./images/logo.png"></a>
+
+
+
+	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu">
+
+	<span class="icon-bar"></span>
+	<span class="icon-bar"></span>
+	<span class="icon-bar"></span>
+
+	</button>
+
+	</div>
+
+	<div class="collapse navbar-collapse" id="menu">
+
+	<ul class="nav navbar-nav navbar-right">
+
+	<li><a href="#">The</a></li>
+	<li><a href="#">Routing</a></li>
+	<li><a href="#">Works</a></li>
+
+	</ul>
+
+	</div>
+
+	</div>
+
+	</nav>
+
+
+
+</section>
+
+</header>
